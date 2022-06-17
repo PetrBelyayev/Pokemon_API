f212c4dd316797a3e780666cf99337e6b27d3125
Petr Belyayev <petrb22@gmail.com>
2022-06-17 15:31:47 -0700

    styling

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const container = document.querySelector('#container');
+// pokemon image api
 const baseURL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+// base modifier pad image api and the number on card
 let baseModifier = (number) => (number <= 999 ? `00${number}` .slice(-3): number);
 
-const pokemon_count = 150;
 
-
+// pokemon data api
 const getPokemon = async (id) => {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    console.log(res.data)
+    console.log(res.data.id)
     makePokemonCard(res.data)
 };
-// official-artwork
+
 getPokemon(1)
 
 
@@ -18,61 +19,68 @@ getPokemon(1)
 const makePokemonCard = (res) => {
     const name = res.name[0].toUpperCase() + res.name.slice(1);
     const type1 = res.types[0].type.name[0].toUpperCase() + res.types[0].type.name.slice(1);
-    const type2 = res.types[1].type.name[0].toUpperCase() + res.types[1].type.name.slice(1);
+    
 
-
-
+    // pokemon card front
     const pokemonCardFront = document.createElement('div');
     pokemonCardFront.classList.add('pokemonCardFront');
-
+    // pokemon card back (experimental)
     const pokemonCardBack = document.createElement('div');
     pokemonCardBack.classList.add('pokemonCardBack');
-    
+    // pokemon title
     const pokemonName = document.createElement('span');
     pokemonName.classList.add('pokemonName')
     pokemonName.innerText = name;
-
+    // pokemon id/number
+    const pokemonId= document.createElement('span');
+    pokemonId.classList.add('pokemonId')
+    pokemonId.innerText = baseModifier(res.id);
+    // pokemon first type info
     const pokemonType1 = document.createElement('span');
     pokemonType1.classList.add(`pokemonType${type1}`)
-    console.log(pokemonType1.classList)
-    
     pokemonType1.innerText = type1;
 
-    
+    // pokemon image (using a seperate api)
+    const pokemonImg = document.createElement('img');
+    pokemonImg.src = `${baseURL}${baseModifier(res.id)}.png`
+    pokemonImg.classList.add('pokemonImg')
+    pokemonCardBack.appendChild(pokemonType1);
 
     // if pokemon has a second type then append that type
     if(res.types[1]) {
+        const type2 = res.types[1].type.name[0].toUpperCase() + res.types[1].type.name.slice(1);
         const pokemonType2 = document.createElement('span');
         pokemonType2.classList.add(`pokemonType${type2}`)
         pokemonType2.innerText = type2;
         pokemonCardBack.appendChild(pokemonType2);
     }
-
-    // applies selector class name based on type
-    // if (pokemonType1.innerText === "grass") {
-    //     console.log("Green")
-    //     pokemonType1.classList.add('pokemonTypeGrass')
-    // }
-
-    // function checkType (type) {
-    //    return pokemonType1.classList.add(`pokemonType${type}`)
-    // }
-    // checkType(res.types[0].type.name)
-
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${baseModifier(res.id)}.png`
-
-    pokemonCardBack.appendChild(pokemonType1);
     
 
     pokemonCardFront.appendChild(pokemonName);
-    pokemonCardFront.appendChild(newImg);
+    pokemonCardFront.appendChild(pokemonImg);
+    pokemonCardFront.appendChild(pokemonId);
 
     container.appendChild(pokemonCardFront);
     container.appendChild(pokemonCardBack);
-
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function makePokemon(i) {
 //     const pokemon = document.createElement('div');
