4f27738963aff543a0fde7a1f3b259b959876ff2
Petr Belyayev <petrb22@gmail.com>
2022-06-16 23:05:03 -0700

    fetch data from pokemon api

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const container = document.querySelector('#container');
+const baseURL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+let baseModifier = (number) => (number <= 999 ? `00${number}` .slice(-3): number);
 
 const pokemon_count = 150;
 
 
-
-
 const getPokemon = async (id) => {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    console.log(res.data.sprites.other)
+    console.log(res.data)
     makePokemonCard(res.data)
 };
 // official-artwork
-getPokemon(1)
+getPokemon(4)
 
 
 
 const makePokemonCard = (res) => {
-    const pokemonCard = document.createElement('div');
-    pokemonCard.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = res.name;
+    const pokemonCardFront = document.createElement('div');
+    pokemonCardFront.classList.add('pokemonCardFront');
+
+    const pokemonCardBack = document.createElement('div');
+    pokemonCardBack.classList.add('pokemonCardBack');
+
+    const pokemonName = document.createElement('span');
+    pokemonName.classList.add('pokemonName')
+    pokemonName.innerText = res.name;
+
+    const pokemonType1 = document.createElement('span');
+    pokemonType1.classList.add('pokemonType')
+    pokemonType1.innerText = res.types[0].type.name;
+
+    // if pokemon has a second type then append that type
+    if(res.types[1]) {
+        const pokemonType2 = document.createElement('span');
+        pokemonType2.classList.add('pokemonType')
+        pokemonType2.innerText = res.types[1].type.name;
+        pokemonCardBack.appendChild(pokemonType2);
+    }
+
     const newImg = document.createElement('img');
-    // newImg.src = `${baseURL}${baseModifier([i])}.png`
-    pokemonCard.appendChild(newImg);
-    pokemonCard.appendChild(label);
-    container.appendChild(pokemonCard);
+    newImg.src = `${baseURL}${baseModifier(res.id)}.png`
+
+    pokemonCardBack.appendChild(pokemonType1);
+    
+
+    pokemonCardFront.appendChild(pokemonName);
+    pokemonCardFront.appendChild(newImg);
+
+    container.appendChild(pokemonCardFront);
+    container.appendChild(pokemonCardBack);
+
+    
 }
 
 // function makePokemon(i) {
