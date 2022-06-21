b84afdbb8063220f22a881d0a0c752edca719b66
Petr Belyayev <petrb22@gmail.com>
2022-06-20 21:33:40 -0700

    add async/await to fetchPokemons and make list of weakness for all pokemon pokemon types

--- app.js
+++ app.js
@@ -4,42 +4,36 @@ const baseURL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 // base modifier pad image api and the number on card
 let baseModifier = (number) => (number <= 999 ? `00${number}` .slice(-3): number);
 
-
-// pokemon data api
+// pokemon api
 const getPokemon = async (id) => {
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    // console.log(res.data.abilities[0].ability)
-    console.log(res.data)
-    makePokemonCard(res.data)
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const res2 = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
     
+    makePokemonCard(res.data, res2.data)
+    // console.log(res.data)
+
 };
 
-// function allGen() {
-//     for(let i = 1; i <= 351; i++) {
-//         getPokemon(i);
-//     }
-// }
+const fetchPokemons = async () => {
+    for(let i = 1; i <= 898; i++) {
+        await getPokemon(i);
+    }
+}
 
-// allGen()
+fetchPokemons()
+// getPokemon(80)
 
-getPokemon(151)
+const makePokemonCard = (res, res2) => {
 
-
-
-
-const makePokemonCard = (res) => {
+    // adds uppercase to labels
+    const name = res.name[0].toUpperCase() + res.name.slice(1);
+    const type1 = res.types[0].type.name[0].toUpperCase() + res.types[0].type.name.slice(1);
 
     // converts decimeters to feet
     let decimeterToFeet = res.height/3.048
-    console.log(decimeterToFeet)
     
-
+    // converts feet in decimal to feet and inches
     let pokemonTotalHeight = Math.floor(decimeterToFeet) + "'" + Math.round((12 * (decimeterToFeet - Math.floor(decimeterToFeet)))) +'"';
-    console.log(pokemonTotalHeight)
-
-    const name = res.name[0].toUpperCase() + res.name.slice(1);
-    const type1 = res.types[0].type.name[0].toUpperCase() + res.types[0].type.name.slice(1);
-    
 
     // pokemon card front
     const pokemonCardFront = document.createElement('div');
@@ -49,7 +43,6 @@ const makePokemonCard = (res) => {
     const pokemonCardBack = document.createElement('div');
     pokemonCardBack.classList.add('pokemonCardBack');
     
-
     // pokemon title
     const pokemonName = document.createElement('span');
     pokemonName.classList.add('pokemonName')
@@ -61,14 +54,10 @@ const makePokemonCard = (res) => {
     pokemonId.innerText = baseModifier(res.id);
 
     // pokemon first type info
-    
     const pokemonType1 = document.createElement('span');
     pokemonType1.classList.add(`pokemonType${type1}`)
     pokemonType1.innerText = type1;
     pokemonType1.style.background = `var(--${type1})`
-
-  
-    
 
     // pokemon image (using a seperate api)
     const pokemonImg = document.createElement('img');
@@ -79,11 +68,14 @@ const makePokemonCard = (res) => {
     // pokemon height after conversion
     const pokemonHeight = document.createElement('span');
     pokemonHeight.classList.add('pokemonHeight');
-    pokemonHeight.innerText = `Height ${pokemonTotalHeight}`;
-    
-    const dualTypes = ['dragon', 'flying', 'ground']
-    // ground and flying type use a 2 color gradient for labels which will conflict with variable "type1"- use this to apply a single type color gradient from root
+    pokemonHeight.innerText = pokemonTotalHeight;
 
+    // pokemon species category
+    const pokemonCategory = document.createElement('span');
+    pokemonCategory.classList.add('pokemonCategory');
+    pokemonCategory.innerText = res2.genera[7].genus.replace('Pokémon', '');
+    
+    // ground and flying type use a 2 color gradient for labels which will conflict with variable "type1"- use this to apply a single type color gradient from root
     if(res.types[0].type.name === 'dragon' ||
         res.types[0].type.name ==='flying' ||
         res.types[0].type.name ==='ground')
@@ -92,12 +84,12 @@ const makePokemonCard = (res) => {
         `-webkit-radial-gradient(center, var(--${type1}Single) 0%, #ffffff 70%)`;
     }
     
-    
-
+    // appended components go here
     pokemonCardFront.appendChild(pokemonName);
     pokemonCardFront.appendChild(pokemonImg);
     pokemonCardFront.appendChild(pokemonId);
     pokemonCardBack.appendChild(pokemonType1);
+
     // if pokemon has a second type then append that type
     if(res.types[1]) {
         const type2 = res.types[1].type.name[0].toUpperCase() + res.types[1].type.name.slice(1);
@@ -108,93 +100,13 @@ const makePokemonCard = (res) => {
         pokemonCardBack.appendChild(pokemonType2);
     }
 
-    
     pokemonCardBack.appendChild(pokemonHeight);
+
     container.appendChild(pokemonCardFront);
     container.appendChild(pokemonCardBack);
+    pokemonCardBack.appendChild(pokemonCategory);
+
+
+   
+
 }
-
-
-// converts total feet in decimal to feet and inches
-let feet = function feetAndInches(decimal) {
-    return Math.floor(decimal) + "'" + Math.round((12 * (decimal - Math.floor(decimal)))) +'"';
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function makePokemon(i) {
-//     const pokemon = document.createElement('div');
-//     pokemon.classList.add('pokemon');
-//     const label = document.createElement('span');
-//     label.innerText = `No. ${i}`;
-//     const newImg = document.createElement('img');
-//     newImg.src = `${baseURL}${baseModifier([i])}.png`
-//     pokemon.appendChild(newImg);
-//     pokemon.appendChild(label);
-//     container.appendChild(pokemon);
-// }
-
-// function allGen() {
-//     for(let i = 1; i <= 905; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// allGen()
-
-// function gen1() {
-//     for(let i = 1; i <= 151; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// // gen1()
-
-
-// function gen2() {
-//     for(let i = 152; i <= 251; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// // gen2()
-
-// function gen3() {
-//     for(let i = 252; i <= 386; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// gen3()
-
-// function gen4() {
-//     for(let i = 387; i <= 493; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// // gen4()
-
-// function gen5() {
-//     for(let i = 494; i <= 649; i++) {
-//         makePokemon(i);
-//     }
-// }
-
-// gen5()
-
---
+++ weakness.js
@@ -0,0 +1,64 @@
+let pokemonWeakness = {
+    type: 'Normal',
+    against: ['Fighting'],
+    type: 'Fire',
+    against: ['Rock', 'Ground', 'Water'],
+    type: 'Water',
+    against: ['Grass', 'Electric'],
+    type: 'Electric',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Grass',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Ice',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Fighting',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Poison',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Ground',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Flying',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Psychic',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Rock',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Ghost',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Dragon',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Dark',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Steel',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Fairy',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    type: 'Bug',
+    against: ['Fire', 'Ice', 'Flying', 'Psychic']
+}
+
+ // let allWeakness = {
+    //     type: 'Grass',
+    //     against: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    //     type: 'Normal',
+    //     against: ['Fighting'],
+    //     type: 'Fire',
+    //     against: ['Rock', 'Ground', 'Water'],
+    //     type: 'Water',
+    //     against: ['Grass', 'Electric'],
+    // };
+
+    // let allWeakness = {
+    //     Grass: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    //     Normal: ['Fighting'],
+    //     Fire: ['Rock', 'Ground', 'Water'],
+    //     Water: ['Grass', 'Electric'],
+    // };
+    
+    //     console.log(allWeakness)
+
+    // if(allWeakness.hasOwnProperty(type1))  {
+    //     console.log('True', type1)
+    // } else {
+    // console.log('Does not exist')
+    // }
